1e89065d5eb27733eb4ce71778a58e5c05fbe702
Krishna Jaybhaye <92299949+matrixninja1@users.noreply.github.com>
2024-07-09 15:10:20 +0530

    Update Demo.java

--- GithubProject/src/com/cjc/Demo.java
+++ GithubProject/src/com/cjc/Demo.java
@@ -3,6 +3,13 @@ package com.cjc;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("main Start");
+		Demo d=new Demo();
+		d.m1();
+	}
+
+	public void m1()
+	{
+		System.out.println("m1 method");
 	}
 
 }
